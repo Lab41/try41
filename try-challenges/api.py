b808f4cc0c1228f000ff63bb59de66c9fbee4ae9
clewis <clewis@iqt.org>
2013-11-12 09:33:55 -0800

    add favicon

--- try-challenges/api.py
+++ try-challenges/api.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import jsonify
 from flask import render_template
 from flask import request
-from flask import jsonify
+from flask import send_from_directory
 
 app = Flask(__name__)
 app.debug = True
@@ -30,6 +31,10 @@ def details(url):
 def robot():
     return render_template("robot.html")
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__ == '__main__':
     import sys, os
     app.run(host="0.0.0.0")
