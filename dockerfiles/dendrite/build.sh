b80d523698336fc182c3bc15a9400984f984730e
Charlie Lewis <clewis@iqt.org>
2014-06-25 15:19:46 +0000

    get assigned ports; dendrite build script

---
+++ dockerfiles/dendrite/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd dendrite-build
+docker build -t lab41/dendrite-build .
+cd ../dendrite-java
+docker build -t lab41/dendrite-java .
+cd ../dendrite-repos
+docker build -t lab41/dendrite-repos .
+cd ../dendrite-cdh5
+docker build -t lab41/dendrite-cdh5 .
+cd ../dendrite-elasticsearch
+docker build -t lab41/dendrite-elasticsearch .
+cd ../dendrite-graphlab
+docker build -t lab41/dendrite-graphlab .
+cd ../dendrite-ungit
+docker build -t lab41/dendrite-ungit .
+cd ../dendrite-snap
+docker build -t lab41/dendrite-snap .
+cd ..
+docker build -t lab41/dendrite .
