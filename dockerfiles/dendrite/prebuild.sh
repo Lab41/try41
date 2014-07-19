ba268872a1951dbd8e8efbfab19381c4ac2ef7e6
Kyle F <kylef@iqt.org>
2014-07-19 11:17:47 -0700

    Alternative prebuild script
    - touch output file to ensure it exists for while loop
    - use single while loop that examines/greps last line of logfile
    - echo progress while dendrite builds

--- dockerfiles/dendrite/prebuild.sh
+++ dockerfiles/dendrite/prebuild.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 /usr/bin/supervisord
+touch /tmp/output
 sleep 5
-mvn -f /Dendrite/pom.xml -Dmaven.repo.local=/root/lib tomcat7:run -Dmaven.tomcat.port=8000 -Dspring.profiles.active=prod -DwithGitHistoryServer=true -Djava.security.egd=file:/dev/./urandom -DskipTests > /tmp/output 2>&1 &
 
-tail -f /tmp/output | while read LOGLINE
-do
-   [[ "${LOGLINE}" == *"INFO: Starting ProtocolHandler [\"http-bio-8000\"]"* ]] && pkill -P $$ tail
-done
+mvn -Dmaven.repo.local=/root/lib tomcat7:run -Dmaven.tomcat.port=8000 -Dspring.profiles.active=prod -DwithGitHistoryServer=true -Djava.security.egd=file:/dev/./urandom -DskipTests > /tmp/output 2>&1 &
 
-tail -f /tmp/output | while read LOGLINE
+__started=false
+while [ "$__started" == "false" ]
 do
-   [[ "${LOGLINE}" == *"INFO: Starting ProtocolHandler [\"http-bio-8000\"]"* ]] && pkill -P $$ tail
+  echo "waiting for dendrite startup and build initial graph..."
+  __lastline=$(tail /tmp/output -n 1)
+  echo -e "startup message: $__lastline\n"
+
+  if [[ "$__lastline" == *"INFO: Starting ProtocolHandler [\"http-bio-8000\"]"* ]]; then
+    __started=true
+  else
+    sleep 5
+  fi
 done
