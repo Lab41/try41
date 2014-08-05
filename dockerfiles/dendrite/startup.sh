2cf4b4f613c3d1757960e00155da69812445e6ee
Kyle F <kylef@iqt.org>
2014-08-05 06:56:15 -0700

    startup.sh: Use NetworkSettings.Ports for port discovery

--- dockerfiles/dendrite/startup.sh
+++ dockerfiles/dendrite/startup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ -z "$SSL" ]; then
-	PORT=$(curl --silent http://172.17.42.1:2375/containers/$HOSTNAME/json | jq '.HostConfig.PortBindings."8448/tcp"[].HostPort')
-	sed "s/config.port/$PORT/g" -i /Dendrite/src/main/nodejs/ungit/bin/ungit
+  PORT=$(curl --silent http://172.17.42.1:2375/containers/$HOSTNAME/json | jq '.NetworkSettings.Ports."8448/tcp"[].HostPort')
 else
         sed "s/config.port/443/g" -i /Dendrite/src/main/nodejs/ungit/bin/ungit
         sed "s|\"\"|\"/dendrite2\"|" -i /Dendrite/src/main/nodejs/ungit/bin/ungit
