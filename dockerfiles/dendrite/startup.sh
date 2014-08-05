8235ea1244584e7db5eab9eea8e929a1040a0ea0
Kyle F <kylef@iqt.org>
2014-08-05 06:57:11 -0700

    startup.sh: Use source/config.js for port updating

--- dockerfiles/dendrite/startup.sh
+++ dockerfiles/dendrite/startup.sh
@@ -2,8 +2,10 @@
 
 if [ -z "$SSL" ]; then
   PORT=$(curl --silent http://172.17.42.1:2375/containers/$HOSTNAME/json | jq '.NetworkSettings.Ports."8448/tcp"[].HostPort')
+  sed "s/port:.*/port: $PORT,/" -i /Dendrite/src/main/nodejs/ungit/source/config.js
 else
-        sed "s/config.port/443/g" -i /Dendrite/src/main/nodejs/ungit/bin/ungit
-        sed "s|\"\"|\"/dendrite2\"|" -i /Dendrite/src/main/nodejs/ungit/bin/ungit
-        sed -i "s/port:.*/port: 443,/g" /Dendrite/src/main/webapp/js/app.js;
+  PORT=443
+  sed "s/port:.*/port: $PORT,/" -i /Dendrite/src/main/nodejs/ungit/source/config.js
+  sed "s/port:.*/port: $PORT,/" -i /Dendrite/src/main/webapp/js/app.js;
+  sed "s|\"\"|\"/dendrite2\"|" -i /Dendrite/src/main/nodejs/ungit/bin/ungit
 fi
